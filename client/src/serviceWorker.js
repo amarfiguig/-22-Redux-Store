b1623f8d87279ba9e8d3643c95aef281fe54ce83
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-21 05:43:06 +0000

    Update serviceWorker.js

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -124,3 +124,102 @@ export function unregister() {
       });
   }
 }
+
+
+
+
+
+
+
+//add
+
+// Check if the current URL hostname is 'localhost', '[::1]' (IPv6 equivalent of localhost), or matches a regex pattern of IP address for localhost.
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '[::1]' ||
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
+
+// Register a service worker for the app if running in production and service worker is supported by the browser.
+export function register(config) {
+  if (
+    process.env.NODE_ENV === 'production' &&
+    'serviceWorker' in navigator
+  ) {
+    // Get the public URL and compare it with the current URL origin.
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
+    // Register the service worker when the page loads.
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      // If running on localhost, check if the service worker is still valid and log a message to the console.
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl, config);
+        navigator.serviceWorker.ready.then(() => {
+          console.log(
+            'This web app is being served cache-first by a service ' +
+            'worker. To learn more, visit https://bit.ly/CRA-PWA'
+          );
+        });
+      } else {
+        // If not running on localhost, register the service worker.
+        registerValidSW(swUrl, config);
+      }
+    });
+  }
+}
+
+// Register the service worker with the given URL and configuration.
+function registerValidSW(swUrl, config) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      // Listen for updates to the service worker.
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            // If an update is available, notify the user and call the 'onUpdate' callback function from the configuration.
+            if (navigator.serviceWorker.controller) {
+              console.log(
+                'New content is available and will be used when all ' +
+                'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+              );
+              if (config && config.onUpdate) {
+                config.onUpdate(registration);
+              }
+            } else {
+              // If there is no controller, the app is in offline mode, so log a message and call the 'onSuccess' callback function from the configuration.
+              console.log('Content is cached for offline use.');
+              if (config && config.onSuccess) {
+                config.onSuccess(registration);
+              }
+            }
+          }
+        };
+      };
+    })
+    .catch(error => {
+      console.error('Error during service worker registration:', error);
+    });
+}
+
+// Check if the service worker is still valid by fetching the URL and checking the content type. If not valid, unregister and reload the page.
+function checkValidServiceWorker(swUrl, config) {
+  fetch(swUrl, {
+    headers: { 'Service-Worker': 'script' },
+  })
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+      if (
+        response.status === 404 ||
+        (contentType != null && contentType
